5407fa63b58560ff597f875fe6afabef78a8f54e
William Palmer <wpalmer@putneyschool.org>
2018-05-24 14:11:39 -0400

    Letter cipher finished

--- substitutionCypher.py
+++ substitutionCypher.py
@@ -1,5 +1,17 @@
+def letterSub(letter, key):
+
+	if (ord(letter) + key > 122):
+		print(ord(letter)+key)
+		letter = chr(ord(letter) + key - 26)
+	else:
+		letter = chr(ord(letter) + key)
+		print(ord(letter)+key)
+	print(letter)
 # key = input("Please input a number key ")
-key = "myKey"
-print("Your key is" key)
+# key = 13
+# print("Your key is" + str(key))
 # message = input("Now please type your message:")
 message = "this is my awesome encoded message"
+# letter = character 1 of message
+letter = "t"
+letterSub(letter,12)
