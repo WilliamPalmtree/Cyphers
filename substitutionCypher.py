b9be186db852c0bfffc08848ac3928495b7cbe5e
William Palmer <wpalmer@putneyschool.org>
2018-05-25 08:44:41 -0400

    Message encoder works

--- substitutionCypher.py
+++ substitutionCypher.py
@@ -3,23 +3,24 @@
 def letterSub(letter, key):
 
 	if (ord(letter) + key > 122):
-		print(ord(letter)+key)
 		letter = chr(ord(letter) + key - 26)
 	else:
 		letter = chr(ord(letter) + key)
-		print(ord(letter)+key)
-	print(letter)
-
+	return letter
 
 # Encode messages takes the message and the key value and returns 
 # the encoded string
 def encodeMessage(message, key):
-
+	encodedMessage = ""
+	for ch in message:
+		newLetter = letterSub(ch, key)
+		encodedMessage = encodedMessage + newLetter
+	print(encodedMessage)
 # key = input("Please input a number key ")
 # key = 13
 # print("Your key is" + str(key))
 # message = input("Now please type your message:")
-message = "this is my awesome encoded message"
+message = "thisismyawesomeencodedmessage"
 # letter = character 1 of message
 letter = "t"
-letterSub(message,12)
+encodeMessage(message, 12)
